a8a63da01c3910d17a2c03863522f9bb6b1562a9
David Alexander Sanders <sandersda@dcsdk12.org>
2016-09-19 12:10:51 -0600

    Adds function

--- arrays.py
+++ arrays.py
@@ -105,17 +105,66 @@ print centered_average([1,16,4,25]) # Expects 10
 
 def unlucky_13(array):
 	sum = 0
+	skip = False
 	for n in array:
-		if n == 13:
-			return sum
+		if skip == False: 
+			if n == 13:
+				skip = True
+			else:
+				sum += n
 		else:
-			sum += n
+			skip = False
 	return sum
 		
 
 print "\n\n\n\nSum 13\n\n\n"
-print unlucky_13([1,2,3,13,1,1,1,2]) # Expects 6
+print unlucky_13([1,2,3,13,1,1,1,2]) # Expects 10
 print unlucky_13([1,2,3,1,1,1,1,2]) # Expects 12
 print unlucky_13([1,2,3,1,1,1,1,3]) # Expects 13
-print unlucky_13([13,2,3,1,1,1,1,3]) # Expects 0
+print unlucky_13([13,2,3,1,1,1,1,3]) # Expects 11
 
+def sum_67(array):
+	hold = 0
+	sum = 0
+	for n in array:
+		if n == 6:
+			hold = 1
+		if n == 7:
+			hold = 0
+		if hold == 0:
+			sum +=n
+
+print sum_67([1,2,6,9000,7,2,10,5]) # Expects 27
+print sum_67([1,1,1,1,6,900000,900000]) # Expects 4
+
+
+def has_22(array):
+	twos = 0
+	for n in array:
+		if n == 2:
+			twos  += 1
+		else:
+			twos = 0
+		if twos == 2:
+			return True
+		if twos < 2:
+			return False
+
+print has_22([2,2,2,2,2,2]) # Expects true
+print has_22([2,4,2,4,2,4]) # Expects false
+print has_22([2,4,2,2,6,7]) # Expects true
+
+def sum_13(a):
+	sum = 0
+	for i in range(0, len(a)):
+		if i == (len(a) - 1):
+			if a[i] != 13:
+				sum += a[i]
+		else:
+			if a[i] == 13:
+				sum -= a[i + 1]
+			else:
+				sum += a[i]
+	return sum
+print "Sum 13"
+print sum_13([1,2,13,9000,3]) # Expects 6
